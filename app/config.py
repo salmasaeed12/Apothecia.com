d7e6fab389cc854e8b50da6ede324a6eba76f4a0
Yahiamostafa11 <yahiawork@gmail.com>
2024-07-16 01:10:55 +0300

    stamony

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
-# mysql+pymysql://<username>:<password>@<hostname>/<database>
-DATABASE_URL = ""
+# mysql+pymysql://<stamony>:<stamony1234>@<localhost>/<Apothecia_db>
+DATABASE_URL = "mysql+pymysql://stamony:stamony1234@localhost/Apothecia_db"
 # python -c "import secrets; print(secrets.token_hex(32))"
 SECRET_KEY = "your_secret_key"
-ALGORITHM = "stamony"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 14400
